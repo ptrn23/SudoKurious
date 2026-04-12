eeb7feef9043fa5b674e3a510140ffb036450f22
ptrn23 <pnecasio23@gmail.com>
2026-04-13 00:27:23 +0800

    fix: lock backend API

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,10 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"], 
+    allow_origins=[
+        "https://sudo-kurious.vercel.app",
+        "http://localhost:3000"
+    ], 
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
