5a0ee437dbe061f7ab49e20904e60911a1be0011
ptrn23 <pnecasio23@gmail.com>
2026-04-26 16:25:01 +0800

    feat: stabilize cnn model

--- backend/ai/cnn_model.py
+++ backend/ai/cnn_model.py
@@ -6,21 +6,27 @@ class SudokuDifficultyPredictor(nn.Module):
     def __init__(self):
         super(SudokuDifficultyPredictor, self).__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=1, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.fc1 = nn.Linear(64 * 9 * 9, 128)
-        self.fc2 = nn.Linear(128, 64)
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, padding=1)
+        self.bn1 = nn.BatchNorm2d(32)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, padding=1)
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 128, kernel_size=3, padding=1)
+        self.bn3 = nn.BatchNorm2d(128)
+        
+        self.fc1 = nn.Linear(128 * 9 * 9, 256)
+        self.dropout = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(256, 64)
         self.fc3 = nn.Linear(64, 1)
 
     def forward(self, x):
-        # x expected shape: [batch_size, 1, 9, 9]
+        x = F.relu(self.bn1(self.conv1(x)))
+        x = F.relu(self.bn2(self.conv2(x)))
+        x = F.relu(self.bn3(self.conv3(x)))
+        x = x.view(x.size(0), -1) 
         
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = x.view(x.size(0), -1)
         x = F.relu(self.fc1(x))
+        x = self.dropout(x)
         x = F.relu(self.fc2(x))
-        
         x = self.fc3(x)
         
         return x
